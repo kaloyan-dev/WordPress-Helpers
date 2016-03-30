b8d1e2b14ff653ebece22e36ac94cf8e0edfe4c4
Kaloyan Ivanov <kaloyan.github@gmail.com>
2016-03-30 21:15:07 +0300

    Initial helpers and filters

---
+++ filters.php
@@ -0,0 +1,10 @@
+<?php
+
+# Restores old comment form layout
+function crb_move_comment_field_to_bottom( $fields ) {
+	$comment_field = $fields['comment'];
+	unset( $fields['comment'] );
+	$fields['comment'] = $comment_field;
+	return $fields;
+}
+add_filter( 'comment_form_fields', 'crb_move_comment_field_to_bottom' );
---
+++ helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+# Helper function for Carbon Fields complex field labels
+# https://github.com/htmlburger/carbon-fields
+function crb_complex_labels( $singular = false, $plural = false ) {
+	if ( ! $singular ) {
+		$singular = __( 'Entry', 'crb' );
+	}
+	if ( ! $plural ) {
+		$plural = $singular . 's';
+	}
+	return array(
+		'singular_name' => $singular,
+		'plural_name'   => $plural,
+	);
+}
+
+# Helper function for WPThumb
+# https://github.com/humanmade/WPThumb
+function crb_wpthumb( $id, $width = 0, $height = 0, $src = false, $crop = true, $classes = '', $echo = true ) {
+	$size = array( 
+		'width'  => $width, 
+		'height' => $height, 
+		'crop'   => $crop, 
+	);
+	
+	$attr = array(
+		'class' => $classes,
+	);
+	if ( $src ) {
+		$image_src = wp_get_attachment_image_src( $id, $size, false, $attr );
+		return $image_src[0];
+	}
+	
+	if ( ! $echo ) {
+		return wp_get_attachment_image( $id, $size, false, $attr );
+	}
+	echo wp_get_attachment_image( $id, $size, false, $attr );
+}

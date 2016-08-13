be8882868af0049fe7189322f7caec664e3ed840
Kaloyan Ivanov <kaloyanxivanov@gmail.com>
2016-08-13 15:18:04 +0300

    Add custom avatar filter

--- filters.php
+++ filters.php
@@ -1,8 +1,8 @@
 <?php
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Restores old comment form layout
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_move_comment_field_to_bottom( $fields ) {
 	$comment_field = $fields['comment'];
 	unset( $fields['comment'] );
@@ -11,12 +11,12 @@ function crb_move_comment_field_to_bottom( $fields ) {
 }
 add_filter( 'comment_form_fields', 'crb_move_comment_field_to_bottom' );
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 #  Override WooCommerce template in a plugin file
 #
 #  Requrements:
 #  - WooCommerce: https://www.woothemes.com/woocommerce/
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_plugin_path() { 
 	return untrailingslashit( plugin_dir_path( __FILE__ ) ); 
 }
@@ -49,3 +49,40 @@ function crb_woocommerce_locate_template( $template, $template_name, $template_p
 	return $template;
 }
 add_filter( 'woocommerce_locate_template', 'crb_woocommerce_locate_template', 10, 3 );
+
+# --------------------------------------------------------------------
+#  Override default WordPress avatar
+#
+#  Reference:
+#  https://codex.wordpress.org/Plugin_API/Filter_Reference/get_avatar
+# --------------------------------------------------------------------
+function crb_custom_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
+	$user = false;
+
+	if ( is_numeric( $id_or_email ) ) {
+
+		$id   = (int) $id_or_email;
+		$user = get_user_by( 'id' , $id );
+
+	} elseif ( is_object( $id_or_email ) ) {
+
+		if ( ! empty( $id_or_email->user_id ) ) {
+			$id   = (int) $id_or_email->user_id;
+			$user = get_user_by( 'id' , $id );
+		}
+
+	} else {
+		$user = get_user_by( 'email', $id_or_email );	
+	}
+
+	if ( $user && is_object( $user ) ) {
+
+		# User avatar logic goes here
+		$avatar = 'YOUR_NEW_IMAGE_URL';
+
+		$avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+	}
+
+	return $avatar;
+}
+add_filter( 'get_avatar', 'crb_custom_avatar', 1, 5 );
--- helpers.php
+++ helpers.php
@@ -1,18 +1,18 @@
 <?php
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Custom array_chunk() that makes X number of chunks
 # instead of chunks with X number of items
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_array_chunk( $array = array(), $chunks = 2 ) {
 	$per_chunk = ceil( count( $array ) / $chunks  );
 
 	return array_chunk( $array, $per_chunk );
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Checks if any of the provided arguments is false
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_check_args() {
 	$valid = true;	
 	$args  = func_get_args();
@@ -27,9 +27,9 @@ function crb_check_args() {
 	return $valid;
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Gets a list of specific post type(s)
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_get_posts_list( $post_type = 'post', $posts_per_page = -1, $orderby = 'title', $order = 'ASC', $additional_args = array() ) {
 	$list = array();
 	
@@ -49,12 +49,12 @@ function crb_get_posts_list( $post_type = 'post', $posts_per_page = -1, $orderby
 	return $list;
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 #  Helper function for Carbon Fields complex field labels
 #
 #  Requrements:
 #  - Carbon Fields: https://github.com/htmlburger/carbon-fields
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_complex_labels( $singular = false, $plural = false ) {
 	if ( ! $singular ) {
 		$singular = __( 'Entry', 'crb' );
@@ -68,12 +68,12 @@ function crb_complex_labels( $singular = false, $plural = false ) {
 	);
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Helper function for WPThumb
 #
 # Requirements:
 # - WPThumb: https://github.com/humanmade/WPThumb
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_wpthumb( $id, $width = 0, $height = 0, $src = false, $crop = true, $classes = '', $echo = true ) {
 	$size = array( 
 		'width'  => $width, 
@@ -95,12 +95,12 @@ function crb_wpthumb( $id, $width = 0, $height = 0, $src = false, $crop = true, 
 	echo wp_get_attachment_image( $id, $size, false, $attr );
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Enqueue WooCommerce price filter slider
 #
 # Requirements:
 # - WooCommerce: https://www.woothemes.com/woocommerce/
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_enqueue_woocommerce_slider() {
 	if ( ! function_exists( 'WC' ) || wp_script_is( 'wc-price-slider', 'enqueue' ) ) {
 		return;
@@ -117,12 +117,12 @@ function crb_enqueue_woocommerce_slider() {
 	wp_enqueue_script( 'wc-price-slider' );
 }
 
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 # Get the minimum and maximum price of products
 #
 # Requirements:
 # - WooCommerce: https://www.woothemes.com/woocommerce/
-# -------------------------------------------------------------
+# --------------------------------------------------------------------
 function crb_get_product_prices( $term_id = false ) {
 	$prices = array(
 		'min' => 1,
